b32e15ad86286817df0e1563be3d7cdd05b28921
Andy Mortimer <aamortimer@users.noreply.github.com>
2014-09-10 13:10:24 +0100

    Update openweather.php

--- openweather.php
+++ openweather.php
@@ -25,3 +25,73 @@
       
     return $xml;
   }
+
+
+  function show_weather($area) {
+    $w = weather($area);
+    
+    if (!$w) return false;
+    ?>
+    <!-- weather -->
+  	<div class="weather-container">
+      <h4><?php echo $area; ?> 5 day Weather Forecast</h4>
+      <ul id="weather">
+        <li>
+          <span>
+            <span class="wearher-icon"><img src="http://openweathermap.org/img/w/<?php echo $w->forecast->time[0]->symbol->attributes()->var; ?>.png" /></span><br />
+      	    <strong class="temp"><?php echo ceil($w->forecast->time[0]->temperature->attributes()->day); ?>&deg;C</strong><br />
+            <small class="meta">
+              <strong>Date</strong> Today<br />
+              <strong>Min</strong> <?php echo ceil($w->forecast->time[0]->temperature->attributes()->min); ?>&deg;C
+              <strong>Max</strong> <?php echo ceil($w->forecast->time[0]->temperature->attributes()->max); ?>&deg;C
+            </small>
+          </span>
+        </li>
+        <li>
+          <span>
+            <span class="wearher-icon"><img src="http://openweathermap.org/img/w/<?php echo $w->forecast->time[1]->symbol->attributes()->var; ?>.png" /></span><br />
+      	    <strong class="temp"><?php echo ceil($w->forecast->time[1]->temperature->attributes()->day); ?>&deg;C</strong><br />
+            <small class="meta">
+              <strong>Date</strong> <?php echo date('l', strtotime($w->forecast->time[1]->attributes()->day)); ?><br />
+              <strong>Min</strong> <?php echo ceil($w->forecast->time[1]->temperature->attributes()->min); ?>&deg;C
+              <strong>Max</strong> <?php echo ceil($w->forecast->time[1]->temperature->attributes()->max); ?>&deg;C
+            </small>
+          </span>
+        </li>
+        <li>
+          <span>
+            <span class="wearher-icon"><img src="http://openweathermap.org/img/w/<?php echo $w->forecast->time[2]->symbol->attributes()->var; ?>.png" /></span><br />
+      	    <strong class="temp"><?php echo ceil($w->forecast->time[2]->temperature->attributes()->day); ?>&deg;C</strong><br />
+            <small class="meta">
+              <strong>Date</strong> <?php echo date('l', strtotime($w->forecast->time[2]->attributes()->day)); ?><br />            
+              <strong>Min</strong> <?php echo ceil($w->forecast->time[2]->temperature->attributes()->min); ?>&deg;C
+              <strong>Max</strong> <?php echo ceil($w->forecast->time[2]->temperature->attributes()->max); ?>&deg;C
+            </small>
+          </span>
+        </li>
+        <li>
+          <span>
+            <span class="wearher-icon"><img src="http://openweathermap.org/img/w/<?php echo $w->forecast->time[3]->symbol->attributes()->var; ?>.png" /></span><br />
+      	    <strong class="temp"><?php echo ceil($w->forecast->time[3]->temperature->attributes()->day); ?>&deg;C</strong><br />
+            <small class="meta">
+              <strong>Date</strong> <?php echo date('l', strtotime($w->forecast->time[3]->attributes()->day)); ?><br />            
+              <strong>Min</strong> <?php echo ceil($w->forecast->time[3]->temperature->attributes()->min); ?>&deg;C
+              <strong>Max</strong> <?php echo ceil($w->forecast->time[3]->temperature->attributes()->max); ?>&deg;C
+            </small>
+          </span>
+        </li>
+        <li>
+          <span>
+            <span class="wearher-icon"><img src="http://openweathermap.org/img/w/<?php echo $w->forecast->time[4]->symbol->attributes()->var; ?>.png" /></span><br />
+      	    <strong class="temp"><?php echo ceil($w->forecast->time[4]->temperature->attributes()->day); ?>&deg;C</strong><br />
+            <small class="meta">
+              <strong>Date</strong> <?php echo date('l', strtotime($w->forecast->time[3]->attributes()->day)); ?><br />            
+              <strong>Min</strong> <?php echo ceil($w->forecast->time[4]->temperature->attributes()->min); ?>&deg;C
+              <strong>Max</strong> <?php echo ceil($w->forecast->time[4]->temperature->attributes()->max); ?>&deg;C
+            </small>	        
+          </span>
+        </li>
+      </ul>
+  	</div>
+  <?php
+  }
